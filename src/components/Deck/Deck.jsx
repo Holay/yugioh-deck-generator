148b4fb67354da46529e6fbad92ddc27e7dede78
Mateo Penagos <mateofer_2@hotmail.com>
2020-05-07 16:23:56 -0500

    improved small screen support

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -7,25 +7,25 @@ function Deck() {
     const [deckName, setDeckName] = useState()
     const [tab, setTab] = useState('deck');
     const { deckList, extraDeckList, reducedDeckList, reducedExtraDeckList, removeCard, downloadDecks } = useContext(DeckListContext)
-    const onChange = (event) =>{
+    const onChange = (event) => {
         setDeckName(event.target.value)
     }
-    const chips = tab === 'deck' ? reducedDeckList.map(card => <Chip removeCard={removeCard} card={card} key={card.name} type="deck"/>) : reducedExtraDeckList.map(card => <Chip removeCard={removeCard} type="extraDeck" card={card} key={card.name} />);
+    const chips = tab === 'deck' ? reducedDeckList.map(card => <Chip removeCard={removeCard} card={card} key={card.name} type="deck" />) : reducedExtraDeckList.map(card => <Chip removeCard={removeCard} type="extraDeck" card={card} key={card.name} />);
     const ready = deckList.length >= 40;
-    return(
-    <div className='deck-list'>
-        <div className="deck-name-container">
-            <input type="text" name="deck-name" className="deck-name" placeholder="Deck Name" onChange={onChange}/>
-        </div>
-        <div className="tabs">
-    <button className={`${tab === 'deck'?'active':''}`} onClick={() => setTab('deck')}>Deck {deckList.length}/60</button>
-    <button className={`${tab === 'extraDeck' ? 'active' : ''}`} onClick={() => setTab('extraDeck')}>Extra Deck {extraDeckList.length}/15</button>
-        </div>
-        <div className="chip-container">
-            {chips}
-        </div>
-            <button onClick={()=>{downloadDecks(deckName)}} disabled={!ready} className={`btn draw-border ${ready? 'ready':''}`} id="export-button">Export to TTS</button>
-    </div>)
+    return (
+        <div className='deck-list'>
+            <div className="deck-name-container">
+                <input type="text" name="deck-name" className="deck-name" placeholder="Deck Name" onChange={onChange} />
+            </div>
+            <div className="tabs">
+                <button className={`${tab === 'deck' ? 'active' : ''}`} onClick={() => setTab('deck')}>Deck {deckList.length}/60</button>
+                <button className={`${tab === 'extraDeck' ? 'active' : ''}`} onClick={() => setTab('extraDeck')}>Extra Deck {extraDeckList.length}/15</button>
+            </div>
+            <div className="chip-container">
+                {chips}
+            </div>
+            <button onClick={() => { downloadDecks(deckName) }} disabled={!ready} className={`btn draw-border ${ready ? 'ready' : ''}`} id="export-button">Export to TTS</button>
+        </div>)
 }
 
 
